fa30dc004ec92da4afee4b92ceab8371cde7e94f
Fred Schoeneman <fred.schoeneman@gmail.com>
2010-09-19 01:41:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handsomefencer::Application.config.secret_token = 'f5a1c7d38128671d4d81e8da68d4cdb25f7f3ae5ebf066f70ebfe7e4e07028e6108f1f97943f783ed356abf3d24d7ae589f5a758caf3b5ca88a06f6a23446d26'
